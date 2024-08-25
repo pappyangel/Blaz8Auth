c9ea365709b648a718e443351d738ea8c168a5ed
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-08-25 17:36:49 +0000

    chore: Update authentication page to fix rendering issue

--- B8Auth1/Program.cs
+++ B8Auth1/Program.cs
@@ -50,7 +50,7 @@ builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuth
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));    
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
