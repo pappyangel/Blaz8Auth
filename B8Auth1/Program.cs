e966b030f782f30a1bc3d1926f2d4ad09eeb9b55
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-08-31 15:28:35 +0000

    chore: Update authentication configuration for Microsoft Account authentication

--- B8Auth1/Program.cs
+++ B8Auth1/Program.cs
@@ -29,15 +29,17 @@ builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuth
 
 builder.Services.AddAuthentication(options =>
 {
-   options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-   options.DefaultChallengeScheme = MicrosoftAccountDefaults.AuthenticationScheme;
-})
-.AddCookie("Identity.External")
+//    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = MicrosoftAccountDefaults.AuthenticationScheme;
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+}) 
 .AddMicrosoftAccount(microsoftOptions =>
 {
    microsoftOptions.ClientId = configuration["Authentication:Microsoft:ClientId"]!;
    microsoftOptions.ClientSecret = configuration["Authentication:Microsoft:ClientSecret"]!;
-});
+})
+.AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -63,9 +65,10 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+
 
 app.UseStaticFiles();
 app.UseAntiforgery();
